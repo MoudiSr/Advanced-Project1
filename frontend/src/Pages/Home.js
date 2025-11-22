f9097cd55a954d8f1134fb39ef352a825e12d685
Mohammad Ahmad <42230460@students.liu.edu.lb>
2025-11-22 20:40:35 +0200

    Updated Home.js: renamed variables and changed heading

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -5,30 +5,30 @@ import RestoThree from "../Assets/RestoThree.jpg";
 import { Link } from "react-router-dom";
 function Home() {
   const images = [Restoo, RestoTwo, RestoThree];
-  const [current, setCurrent] = useState(0);
+  const [slideIndex, setCurrent] = useState(0);
 
-  const nextImage = () => setCurrent((prev) => (prev + 1) % images.length);
-  const prevImage = () => setCurrent((prev) => (prev - 1 + images.length) % images.length);
+  const nextSlide = () => setCurrent((prev) => (prev + 1) % images.length);
+  const prevSlide = () => setCurrent((prev) => (prev - 1 + images.length) % images.length);
 
   return (
     <div className="container text-center py-4">
-      <h1 className="text-danger">Welcome to My Restaurant</h1>
+      <h1 className="text-danger">Welcome to Mohammad's Restaurant</h1>
       <p className="lead mx-auto" style={{ maxWidth: "600px" }}>
         Enjoy our delicious meals delivered fresh to your door.
       </p>
 
       <div className="position-relative mx-auto" style={{ maxWidth: "500px" }}>
         <img 
-          src={images[current]}
+          src={images[slideIndex]}
           className="img-fluid rounded"
           style={{ height: "280px", width: "100%", objectFit: "cover" }}
         />
 
-        <button className="btn btn-danger position-absolute top-50 start-0 translate-middle-y" onClick={prevImage}>
+        <button className="btn btn-danger position-absolute top-50 start-0 translate-middle-y" onClick={prevSlide}>
           Prev
         </button>
 
-        <button className="btn btn-danger position-absolute top-50 end-0 translate-middle-y" onClick={nextImage}>
+        <button className="btn btn-danger position-absolute top-50 end-0 translate-middle-y" onClick={nextSlide}>
           Next
         </button>
       </div>
